79ad5f4a7bbcbe5e97e61253cb49ce3f3c781fc2
divyaanshupal <divyanshuwork03@gmail.com>
2026-05-26 22:57:14 +0530

    Pulled latest main changes

--- stock_archery/client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ stock_archery/client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_core
 import firebase_messaging
 import flutter_inappwebview_macos
 import package_info_plus
+import path_provider_foundation
 import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
@@ -25,6 +26,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
